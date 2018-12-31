650a3ae9f56dd32958cf045aea98b61688a4f620
soblin <hilo.mamoru@gmail.com>
2018-12-31 23:42:48 +0900

    [test]; make test

--- src/runtime2.cpp
+++ src/runtime2.cpp
@@ -7,10 +7,10 @@
 
 int main(){
   using namespace project_name;
-  ClassLibrary1 lib1();
-  ClassLibrary2 lib2();
-  ClassLibrary3 lib3();
-  ClassLibrary4 lib4();
+  ClassLibrary1 lib1;
+  ClassLibrary2 lib2;
+  ClassLibrary3 lib3;
+  ClassLibrary4 lib4;
   
   std::cout << "Hello World" << std::endl;
 }
--- test/test1.cpp
+++ test/test1.cpp
@@ -1,6 +1,16 @@
 #include <gtest/gtest.h>
+#include <project_name/lib1/lib1_header1.h>
+#include <project_name/lib1/lib1_header2.h>
+#include <project_name/lib2/lib2_header1.h>
+#include <project_name/lib2/lib2_header2.h>
 
 TEST(test1, test1){
+  using namespace project_name;
+  ClassLibrary1 lib1;
+  ClassLibrary2 lib2;
+  ClassLibrary3 lib3;
+  ClassLibrary4 lib4;
+  
   int val = 123;
   ASSERT_EQ(val, 123);
 }
